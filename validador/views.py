203c11caac4920f7ec438f93ab202cfc6077bcfe
pmoraes230 <pmoraes230nascimento@gmail.com>
2025-08-06 17:29:44 -0300

    tela validador sem back

--- validador/views.py
+++ validador/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from home.views import get_user_profile
 
 # Create your views here.
+def validador(request):
+    context = get_user_profile(request)
+    return render(request, "validador.html", context)
